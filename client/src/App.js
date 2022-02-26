11a93061662ae9b1a8dd36e2b1b29f1a4ef9d128
charlesmariga <charlesmariga37@gmail.com>
2022-02-26 21:49:45 +0300

    Started navbar setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import "./App.scss";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./containers";
+import { Navbar } from "./components";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 }
