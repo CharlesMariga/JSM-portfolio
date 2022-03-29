3a839e81c2a6fad445706ed4e07ebee05c500f19
charlesmariga <charlesmariga37@gmail.com>
2022-03-29 15:10:14 +0300

    Finished the works section of the page

--- client/src/containers/Work/Work.jsx
+++ client/src/containers/Work/Work.jsx
@@ -19,7 +19,19 @@ const Work = () => {
     });
   }, []);
 
-  const handleWorkFilter = () => {};
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+      if (item === "All") {
+        setFilterWork(works);
+      } else {
+        setFilterWork(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
 
   return (
     <>
@@ -31,7 +43,7 @@ const Work = () => {
           (item, index) => (
             <div
               key={index}
-              onClick={() => handleWorkFilter()}
+              onClick={() => handleWorkFilter(item)}
               className={`app__work-filter-item app__flex p-text ${
                 activeFilter === item ? "item-active" : ""
               }`}
